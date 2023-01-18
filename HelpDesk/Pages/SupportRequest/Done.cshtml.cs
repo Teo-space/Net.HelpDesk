eeae9bd5b18235694a2bfc6b15b2b7192491bda6
iTeo <barmin.andrey.nnov@gmail.com>
2023-01-18 23:27:49 +0300

    Added Take, Delete, Done Pages

---
+++ HelpDesk/Pages/SupportRequest/Done.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelpDesk.Pages.SupportRequest
+{
+    public class DoneModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
